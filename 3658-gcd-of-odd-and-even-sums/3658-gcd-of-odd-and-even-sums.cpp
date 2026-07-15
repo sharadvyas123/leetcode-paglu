e5ca6234e93b9fa135ef3aaaaed9d852f0a9e350
sharadvyas123 <sharadvyas132@gmail.com>
2026-07-15 19:39:39 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.02%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        return n;
+        // return n;
+        int e = 0 ;
+        int o = 0;
+
+        for(int num = 1 ; num <= 2*n  ; num++){
+            if(num%2 == 0){
+                e += num;
+            }
+            else{
+                o += num;
+            }
+        }
+
+        return g(e , o);
+    }
+private:
+    int g(int a , int b){
+        if(a > b) return g(a-b , b);
+        else if (b > a) return g(a , b -a);
+        return b;
     }
 };
