0912659fdfee9c31eae955f2835e104e772da3b4
sharadvyas123 <sharadvyas132@gmail.com>
2026-09-04 16:56:32 +0530

    Time: 0 ms (100%), Space: 9 MB (22.83%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string res;
+        int depth = 0;
+
+        for(char c : s){
+            if(c == '('){
+                if (depth > 0){
+                    res += c;
+                }
+                depth++;
+            }
+            else{
+                depth--;
+                if(depth > 0){
+                    res += c;
+                }
+            }
+        }
+        return res;
+    }
+};
